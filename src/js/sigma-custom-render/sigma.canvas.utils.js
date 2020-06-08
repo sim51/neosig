2cfc496cbe863e048835aa4dbf73859771eb9d83
Benoit Simard <contact@bsimard.com>
2020-06-08 12:53:07 +0200

    Correct arrow display with zoom

--- src/js/sigma-custom-render/sigma.canvas.utils.js
+++ src/js/sigma-custom-render/sigma.canvas.utils.js
@@ -1,35 +1,44 @@
-;(function() {
-  'use strict';
+(function() {
+  "use strict";
 
-  sigma.utils.pkg('sigma.canvas.utils');
+  sigma.utils.pkg("sigma.canvas.utils");
 
-  sigma.canvas.utils.drawCircle = function(context, x, y, size, color){
+  sigma.canvas.utils.drawCircle = function(context, x, y, size, color) {
     context.fillStyle = color;
     context.beginPath();
     context.arc(x, y, size, 0, Math.PI * 2, true);
     context.closePath();
     context.fill();
-  }
+  };
 
   sigma.canvas.utils.drawNodeIcon = function(context, node, prefix) {
-    var color = node.icon.color || '#000';
+    var color = node.icon.color || "#000";
     var scale = node.icon.scale || 0.7;
-    var text = String.fromCharCode("0x" + node.icon.name)  || '?';
-    var nodeSize = node[prefix + 'size'];
-    var nodeX = node[prefix + 'x'];
-    var nodeY = node[prefix + 'y'];
+    var text = String.fromCharCode("0x" + node.icon.name) || "?";
+    var nodeSize = node[prefix + "size"];
+    var nodeX = node[prefix + "x"];
+    var nodeY = node[prefix + "y"];
     var fontSize = Math.round(scale * nodeSize);
 
     context.save();
     context.fillStyle = color;
-    context.font = '' + fontSize + 'px fontawesome';
-    context.textAlign = 'center';
-    context.textBaseline = 'middle';
+    context.font = "" + fontSize + "px fontawesome";
+    context.textAlign = "center";
+    context.textBaseline = "middle";
     context.fillText(text, nodeX, nodeY);
     context.restore();
   };
 
-  sigma.canvas.utils.drawArrow = function(context, aX, aY, bX, bY, cX, cY, color) {
+  sigma.canvas.utils.drawArrow = function(
+    context,
+    aX,
+    aY,
+    bX,
+    bY,
+    cX,
+    cY,
+    color
+  ) {
     context.fillStyle = color;
     context.beginPath();
     context.moveTo(aX, aY);
@@ -38,28 +47,79 @@
     context.lineTo(aX, aY);
     context.closePath();
     context.fill();
-  }
+  };
 
-  sigma.canvas.utils.drawSelfEdgeArrow = function(context, nodeX, nodeY, nodeSize, color, size, order) {
-    var controlPoint = sigma.canvas.utils.getSelfLoopControlPoints(nodeX, nodeY, nodeSize, size, order);
+  sigma.canvas.utils.drawSelfEdgeArrow = function(
+    context,
+    nodeX,
+    nodeY,
+    nodeSize,
+    color,
+    size,
+    order
+  ) {
+    var controlPoint = sigma.canvas.utils.getSelfLoopControlPoints(
+      nodeX,
+      nodeY,
+      nodeSize,
+      size,
+      order
+    );
 
     context.strokeStyle = color;
     context.lineWidth = size;
     context.beginPath();
     context.moveTo(nodeX, nodeY);
-    context.bezierCurveTo(controlPoint.x2, controlPoint.y2, controlPoint.x1, controlPoint.y1, nodeX, nodeY);
+    context.bezierCurveTo(
+      controlPoint.x2,
+      controlPoint.y2,
+      controlPoint.x1,
+      controlPoint.y1,
+      nodeX,
+      nodeY
+    );
     context.stroke();
 
-    var d = Math.sqrt(Math.pow(nodeX - controlPoint.x1, 2) + Math.pow(nodeY - controlPoint.y1, 2));
-    var aX = controlPoint.x1 + (nodeX - controlPoint.x1) * (d - size * 2 - nodeSize) / d;
-    var aY = controlPoint.y1 + (nodeY - controlPoint.y1) * (d - size * 2 - nodeSize) / d;
-    var vX = (nodeX - controlPoint.x1) * nodeSize / d;
-    var vY = (nodeY - controlPoint.y1) * nodeSize / d;
-    sigma.canvas.utils.drawArrow(context, (aX + vX), (aY + vY), (aX + vY * 0.6), (aY - vX * 0.6), (aX - vY * 0.6), (aY + vX * 0.6), color);
-  }
+    var aSize = size * 4;
+    var aX =
+      controlPoint.x +
+      ((targetX - controlPoint.x) * (d - aSize - targetSize)) / d;
+    var aY =
+      controlPoint.y +
+      ((targetY - controlPoint.y) * (d - aSize - targetSize)) / d;
+    var vX = ((targetX - controlPoint.x) * aSize) / d;
+    var vY = ((targetY - controlPoint.y) * aSize) / d;
+    sigma.canvas.utils.drawArrow(
+      context,
+      aX + vX,
+      aY + vY,
+      aX + vY * 0.5,
+      aY - vX * 0.5,
+      aX - vY * 0.5,
+      aY + vX * 0.5,
+      color
+    );
+  };
 
-  sigma.canvas.utils.drawEdgeArrow = function(context, sourceX, sourceY, targetX, targetY, targetSize, color, size, order) {
-    var controlPoint = sigma.canvas.utils.getQuadraticControlPoint(sourceX, sourceY, targetX, targetY, size, order);
+  sigma.canvas.utils.drawEdgeArrow = function(
+    context,
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+    targetSize,
+    color,
+    size,
+    order
+  ) {
+    var controlPoint = sigma.canvas.utils.getQuadraticControlPoint(
+      sourceX,
+      sourceY,
+      targetX,
+      targetY,
+      size,
+      order
+    );
 
     context.strokeStyle = color;
     context.lineWidth = size;
@@ -68,14 +128,31 @@
     context.quadraticCurveTo(controlPoint.x, controlPoint.y, targetX, targetY);
     context.stroke();
 
-    var d = Math.sqrt(Math.pow(targetX - controlPoint.x, 2) + Math.pow(targetY - controlPoint.y, 2));
-    var aX = controlPoint.x + (targetX - controlPoint.x) * (d - size * 7 - targetSize) / d;
-    var aY = controlPoint.y + (targetY - controlPoint.y) * (d - size * 7 - targetSize) / d;
-    var vX = (targetX - controlPoint.x) * targetSize / d;
-    var vY = (targetY - controlPoint.y) * targetSize / d;
-    sigma.canvas.utils.drawArrow(context, (aX + vX), (aY + vY), (aX + vY * 0.6), (aY - vX * 0.6), (aX - vY * 0.6), (aY + vX * 0.6), color);
-  }
+    var d = Math.sqrt(
+      Math.pow(targetX - controlPoint.x, 2) +
+        Math.pow(targetY - controlPoint.y, 2)
+    );
+    var aSize = size * 4;
+    var aX =
+      controlPoint.x +
+      ((targetX - controlPoint.x) * (d - aSize - targetSize)) / d;
+    var aY =
+      controlPoint.y +
+      ((targetY - controlPoint.y) * (d - aSize - targetSize)) / d;
+    var vX = ((targetX - controlPoint.x) * aSize) / d;
+    var vY = ((targetY - controlPoint.y) * aSize) / d;
 
+    sigma.canvas.utils.drawArrow(
+      context,
+      aX + vX,
+      aY + vY,
+      aX + vY * 0.5,
+      aY - vX * 0.5,
+      aX - vY * 0.5,
+      aY + vX * 0.5,
+      color
+    );
+  };
 
   /**
    * Reset the canvas context with default value.
@@ -87,8 +164,8 @@
     context.shadowOffsetY = 0;
     context.shadowBlur = 0;
     context.shadowColor = 0;
-    context.fillStyle = '#000';
-  }
+    context.fillStyle = "#000";
+  };
 
   /**
    * Return the control point coordinates for a quadratic bezier curve.
@@ -101,11 +178,22 @@
    * @param  {number} order    Modifier for the amplitude of the curve.
    * @return {x,y}             The control point coordinates.
    */
-  sigma.canvas.utils.getQuadraticControlPoint = function(x1, y1, x2, y2, edgeSize, order) {
+  sigma.canvas.utils.getQuadraticControlPoint = function(
+    x1,
+    y1,
+    x2,
+    y2,
+    edgeSize,
+    order
+  ) {
     order = order || 0;
     return {
-      x: (x1 + x2) / 2 + (y2 - y1) / (60 / (5 + order * Math.log(edgeSize + 1) * 5)),
-      y: (y1 + y2) / 2 + (x1 - x2) / (60 / (5 + order * Math.log(edgeSize + 1) * 5))
+      x:
+        (x1 + x2) / 2 +
+        (y2 - y1) / (60 / (5 + order * Math.log(edgeSize + 1) * 5)),
+      y:
+        (y1 + y2) / 2 +
+        (x1 - x2) / (60 / (5 + order * Math.log(edgeSize + 1) * 5))
     };
   };
 
@@ -121,14 +209,19 @@
    * @param  {number} order    Modifier to the loop size.
    * @return {x1,y1,x2,y2}     The coordinates of the two control points.
    */
-  sigma.canvas.utils.getSelfLoopControlPoints = function(x , y, nodeSize, edgeSize, order) {
+  sigma.canvas.utils.getSelfLoopControlPoints = function(
+    x,
+    y,
+    nodeSize,
+    edgeSize,
+    order
+  ) {
     order = order || 0;
     return {
-      x1: x - (nodeSize * (order+1)) * Math.log(edgeSize + 1),
+      x1: x - nodeSize * (order + 1) * Math.log(edgeSize + 1),
       y1: y,
       x2: x,
-      y2: y + (nodeSize * (order+1) ) * Math.log(edgeSize + 1)
+      y2: y + nodeSize * (order + 1) * Math.log(edgeSize + 1)
     };
   };
-
 })();
