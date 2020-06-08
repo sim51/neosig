404e0c8be2b55a6eb4458ea18626940d873dd11c
Benoit Simard <contact@bsimard.com>
2020-06-08 12:51:33 +0200

    Adding license & bump version

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,98 @@
-var webpack = require( 'webpack' );
-var path = require( 'path' );
-var argv = require( 'yargs' ).argv;
-var package = require( './package.json' );
-var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
-var UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+var webpack = require("webpack");
+var path = require("path");
+var argv = require("yargs").argv;
+var package = require("./package.json");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 var DEBUG = true;
-if ( process.env.NODE_ENV == 'production' )
-  DEBUG = false;
+if (process.env.NODE_ENV == "production") DEBUG = false;
 
 // Paths
-var basePath = path.resolve( __dirname );
+var basePath = path.resolve(__dirname);
 var paths = {
   base: basePath,
-  nodeModules: path.join( basePath, 'node_modules' ),
-  app: path.join( basePath, 'src' ),
-  index: path.join( basePath, 'src/index.html' ),
-  build: path.join( basePath, 'docs' )
+  nodeModules: path.join(basePath, "node_modules"),
+  app: path.join(basePath, "src"),
+  index: path.join(basePath, "src/index.html"),
+  build: path.join(basePath, "docs")
 };
 
 var wConfig = {
   entry: {
-    neosig: path.join( paths.app, 'js/index' )
+    neosig: path.join(paths.app, "js/index")
   },
   output: {
-    filename: '[name]-' + package.version + '.js',
+    filename: "[name]-" + package.version + ".js",
     path: paths.build
   },
   module: {
     rules: [
-      { test: /\.js$/, loader: 'eslint-loader', enforce: "pre", include: path.resolve( 'src' ) },
+      {
+        test: /\.js$/,
+        loader: "eslint-loader",
+        enforce: "pre",
+        include: path.resolve("src")
+      },
       // Enable ES6 support
-      { test: /\.js?$/, exclude: [ paths.nodeModules ], loaders: [ 'babel-loader' ] },
+      {
+        test: /\.js?$/,
+        exclude: [paths.nodeModules],
+        loaders: ["babel-loader"]
+      },
       //load sigma as global
-      { test: /.*\/sigma.+\.js$/, use: 'imports-loader?this=>window' }
+      { test: /.*\/sigma.+\.js$/, use: "imports-loader?this=>window" }
     ]
   },
   externals: {
-    neo4j: 'neo4j'
+    neo4j: "neo4j"
   },
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
-    new HtmlWebpackPlugin( {
-      template: './src/index.html',
-      inject: 'head'
-    } )
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: "head"
+    })
   ],
   cache: DEBUG,
-  devtool: ( DEBUG ? '#inline-source-map' : false ),
+  devtool: DEBUG ? "#inline-source-map" : false
 };
 
-if ( !DEBUG )
+if (!DEBUG)
   wConfig.plugins.push(
-    new webpack.DefinePlugin( {
-      'process.env': {
-        'NODE_ENV': process.env.NODE_ENV
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: process.env.NODE_ENV
       }
-    } )
+    })
   );
-if ( DEBUG ) {
+if (DEBUG) {
   // Entry points - In production we'll have only the `main.jsx` entry.
   // However in DEBUG, we'll enable Webpacks "Hot Module Replacement"
   // functionality for fast development!
   var entries = {};
-  var defaultEntries = [ 'webpack-dev-server/client?http://localhost:8080', 'webpack/hot/only-dev-server' ];
+  var defaultEntries = [
+    "webpack-dev-server/client?http://localhost:8080",
+    "webpack/hot/only-dev-server"
+  ];
   // Adding default entries
-  entries[ 'neosig' ] = defaultEntries.concat( wConfig.entry.neosig );
+  entries["neosig"] = defaultEntries.concat(wConfig.entry.neosig);
   wConfig.entry = entries;
 
   // Hot Module Replacement ftw
-  wConfig.plugins = wConfig.plugins.concat( [
+  wConfig.plugins = wConfig.plugins.concat([
     new webpack.HotModuleReplacementPlugin(),
-    new UglifyJsPlugin( {
+    new UglifyJsPlugin({
       test: /\.js($|\?)/i,
       uglifyOptions: {
         comments: false
       }
-    } )
-  ] );
+    })
+  ]);
   wConfig.devServer = {
-    hot: true
-  }
+    hot: true,
+    disableHostCheck: true
+  };
 }
 
 module.exports = wConfig;
